3320b27c9a3a49849d58795a9f77a7fd9eaed1c4
tdrudi <145171710+tdrudi@users.noreply.github.com>
2024-02-29 18:14:09 -0500

    Add files via upload

---
+++ bigO.js
@@ -0,0 +1,59 @@
+**Step One: Simplifying Expressions**
+1. O(n + 10) = O(n)
+
+2. O(100 * n) = O(n)
+
+3. O(25) = O(1)
+
+4. O(n^2 + n^3) = O(n^3)
+
+5. O(n + n + n + n) = O(n)
+
+6. O(1000 * log(n) + n) = O(n)
+
+7. O(1000 * n * log(n) + n) = O(n log n)
+
+8. O(2^n + n^2) = O(2^n)
+
+9. O(5 + 3 + 1) = O(1)
+
+10. O(n + n^(1/2) + n^2 + n * log(n)^10) = O(n^2)
+
+
+Step Two: Calculating Time Complexity
+1. Time Complexity: O(n)
+
+2. Time Complexity: O(n)
+
+3. Time Complexity: O(1)
+
+4. Time Complexity: O(n)
+
+5. Time Complexity: O(n^2)
+
+6. Time Complexity: O(n)
+
+**Part 3 - short answer**
+1. True: n^2 + n is O(n^2).
+
+2. True: n^2 * n is O(n^3).
+
+3. followingalse: n^2 + n is O(n).
+
+4. What’s the time complexity of the .indexOf array method? O(n)
+
+5. What’s the time complexity of the .includes array method? O(n)
+
+6. What’s the time complexity of the .forEach array method? O(n)
+
+7. What’s the time complexity of the .sort array method? O(n log n)
+
+8. What’s the time complexity of the .unshift array method? O(n)
+
+9. What’s the time complexity of the .push array method? O(1)
+
+10. What’s the time complexity of the .splice array method? O(n)
+
+11. What’s the time complexity of the .pop array method? O(1)
+
+12. What’s the time complexity of the Object.keys() function? O(n)
